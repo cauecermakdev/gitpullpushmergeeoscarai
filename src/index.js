eb78f66611d74593e47dbb93e9dc053f98d04b2b
mcrestani <marianascrestani@gmail.com>
2023-02-27 18:59:02 -0300

    feat: alterations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import route from "./routes/index.js";
-import dotenv from "dotenv";
-dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+/// testeeee
 
 app.use(route);
 
+
+
+//lalallalal
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
