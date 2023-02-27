1316fb2d87728a3b908891c4de2f8f32d2cd7eab
mcrestani <marianascrestani@gmail.com>
2023-02-27 19:06:04 -0300

    feat: joi

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import joi from "joi"
 import cors from "cors";
 import route from "./routes/index.js";
+import
 
 const app = express();
 app.use(express.json());
@@ -12,5 +14,13 @@ app.use(route);
 
 
 //lalallalal
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+export const newCustomerSchema = joi.object({
+    name: joi.string().min(3).required(),
+    phone: joi.string().alphanum().min(10).max(11).required(),
+    cpf: joi.string().alphanum().min(11).max(11).required(),
+    birthday: joi.date().required(),
+  });
